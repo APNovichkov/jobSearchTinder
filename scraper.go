ebb72fa56c989db7f8cb39a2f16d00ecc65678c3
Andrey Novichkov <andrey.novichkov@gmail.com>
2021-03-04 01:26:13 -0800

    [add] Adding concurrency to the application

--- scraper.go
+++ scraper.go
@@ -20,24 +20,36 @@ type JobListing struct{
 	Origin string   `json:"origin"`
 }
 
+
+
 func RunScraper() ([]JobListing){
 	log.Info("Running Scraper Module")
 
+	
 	totalJobListings := []JobListing{}
+	jobListingsChan := make(chan JobListing)
+	
 
-	ycJobListings, err := getYCJobListings(4)
-	if err != nil{
-		log.Panic("There was an error getting YC Job Listings")
+	go getYCJobListings(4, jobListingsChan)
+
+	// ycJobListings, err := getYCJobListings(4)
+	// if err != nil{
+	// 	log.Panic("There was an error getting YC Job Listings")
+	// }
+
+	for jobListing := range jobListingsChan{
+		totalJobListings = append(totalJobListings, jobListing)
+		fmt.Println("Recieved a new job listing")
 	}
 
-	totalJobListings = append(totalJobListings, ycJobListings...)
+	// totalJobListings = append(totalJobListings, ycJobListings...)
 
-	indeedJobListings, err := getIndeedJobListings(1)
-	if err != nil{
-		log.Panic("There was an error getting Indeed Job Listing")
-	}
+	// indeedJobListings, err := getIndeedJobListings(1)
+	// if err != nil{
+	// 	log.Panic("There was an error getting Indeed Job Listing")
+	// }
 
-	totalJobListings = append(totalJobListings, indeedJobListings...)
+	// totalJobListings = append(totalJobListings, indeedJobListings...)
 
 	return totalJobListings
 }	
@@ -118,9 +130,79 @@ func getIndeedJobListings(numPages int) ([]JobListing, error){
 	return jobListings, nil
 }
 
+// YC Handler
+func getYCJobListings(numPages int, jobListingsChan chan<- JobListing){
+	// Get Job Listings from the YCombinator hacker news jobs page
+
+	// create context
+	log.Info("Initializing Context")
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	const YcOrigin string = "https://news.ycombinator.com/jobs"
+
+	// Define output array
+	// jobListings := []JobListing{}
+
+	// Add timeout to context
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	// Navigate to page
+	if err := chromedp.Run(ctx, chromedp.Navigate(YcOrigin)); err != nil {
+		panic("Error getting to yc url")
+	}
+
+	for i := 0; i < numPages; i++ {
+		log.Info(fmt.Sprintf("Looking at page #%v", i+1))
+
+		// Scrape Posting titles data
+		var postingTitles []*cdp.Node
+		if err := chromedp.Run(ctx, chromedp.Nodes(`.storylink`, &postingTitles)); err != nil {
+			panic("Error getting to job posting object: %v")
+		}
+
+		// Scrape Posting dates data
+		var postingDates []*cdp.Node
+		if err := chromedp.Run(ctx, chromedp.Nodes(`.age a`, &postingDates)); err != nil {
+			panic("Error getting jon posting dates: %v")
+		}
+
+		// Check if lengths of these two are the same
+		if len(postingTitles) != len(postingDates) {
+			panic("Length of posting titles and dates do not align!!")
+		}
+
+		// Parse data into a new struct and append to output array
+		for i := 0; i < len(postingTitles); i++ {
+			newListing := JobListing{
+				Title: postingTitles[i].Children[0].NodeValue,
+				Company: "NA",
+				Location: "NA",
+				Url: postingTitles[i].AttributeValue("href"),
+				Age: postingDates[i].Children[0].NodeValue,
+				IntAge: ConvertStringDateToInt(postingDates[i].Children[0].NodeValue),
+				Origin: YcOrigin,
+			}
+
+			jobListingsChan <- newListing
+			// jobListings = append(jobListings, newListing)
+		}
+
+		// Click on More link
+		log.Info(fmt.Sprintf("Clicking on More link"))
+		if err := chromedp.Run(ctx, chromedp.Click(`.morelink`, chromedp.NodeVisible)); err != nil {
+			panic("Error clicking on 'More' link")
+		}
+	}
+
+	close(jobListingsChan)
+
+}
+
 
 // YC Handler
-func getYCJobListings(numPages int) ([]JobListing, error){
+func getYCJobListings__(numPages int) ([]JobListing, error){
 	// Get Job Listings from the YCombinator hacker news jobs page
 
 	// create context
